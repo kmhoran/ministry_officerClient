3c5dd568507deec4d099158469f4d8a6888d7760
kmhoran <kevin.michael.horan@gmail.com>
2018-06-07 07:08:58 -0700

    but seriously, add the build info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'angular-calendar';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { ModalModule } from 'ngx-bootstrap/modal';
+// import {
+//   startOfDay,
+//   endOfDay,
+//   subDays,
+//   addDays,
+//   endOfMonth,
+//   isSameDay,
+//   isSameMonth,
+//   addHours
+// } from 'date-fns';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { NavigationComponent } from './shared/navigation.component';
+import { MonthCalendarComponent } from './calendar/month-calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
     NavigationComponent,
+    MonthCalendarComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     CalendarModule.forRoot(),
     CollapseModule.forRoot(),
+    ModalModule.forRoot(),
     RouterModule.forRoot([
       {path: 'calendar', component: CalendarComponent},
       {path: '', redirectTo: 'calendar', pathMatch: 'full'},
